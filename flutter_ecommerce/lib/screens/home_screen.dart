7ea532f88742b097d99139d7a5c96fad53050ef9
Gra1gkolodziejczyk <graig.kolodziejczyk@ynov.com>
2023-02-06 10:29:27 +0100

    all files + loader + homePage + assets & directory

---
+++ flutter_ecommerce/lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Home Page"),
+          ],
+        ),
+      ),
+    );
+  }
+}
