9f33aedf474d86ca03c5e22b689ec583566114d2
Gra1gkolodziejczyk <graig.kolodziejczyk@ynov.com>
2023-02-13 09:25:17 +0100

    try to fetch api for switch screen automatic

--- flutter_ecommerce/lib/main.dart
+++ flutter_ecommerce/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_ecommerce/screens/home_screen.dart';
 import 'package:flutter_ecommerce/screens/loader.dart';
+import 'package:http/http.dart' as http;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,15 +10,58 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+Future<Data> fetchData() async {
+  final response = await http.get(Uri.parse('https://pokebuildapi.fr/api/v1/pokemon'));
+  if (response.statusCode == 200) {
+    print("coucou");
+  } else {
+    throw Exception('Failed to load Data');
+  }
+}
+
+class Data {
+  final int userId;
+
+  const Data({
+    required this.userId,
+  });
+}
+
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  late Future<Data> futurData;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Loader(),
+          child: Center(
+            child: FutureBuilder<Data>(
+              future: fetchData(),
+              builder: (context, snapshot) {
+                if(snapshot.hasData) {
+                  return const HomePage();
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return const Loader();
+              },
+            ),
+          ),
         ),
       )
     );
--- flutter_ecommerce/lib/screens/loader.dart
+++ flutter_ecommerce/lib/screens/loader.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/screens/home_screen.dart';
 import 'package:lottie/lottie.dart';
 
 class Loader extends StatelessWidget {
@@ -6,13 +7,39 @@ class Loader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          margin: const EdgeInsets.only(top: 100),
+    return MaterialApp( 
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
           child: Column(
             children: [
-              Lottie.asset('assets/41256-skateboarding.json'),
+              Lottie.asset('assets/41256-skateboarding.json', 
+                onLoaded: (comp) {
+                  const Duration(seconds: 5);
+                },
+              ),
+              const Text("Bienvenue sur ShopTaBoard", 
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.black,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                      MaterialPageRoute(builder: (context) => const HomePage(),
+                    ),
+                  );
+                },
+                label: const Text("HomePage"),
+                icon: const Icon(Icons.arrow_forward),
+              ),
             ],
           ),
         ),
