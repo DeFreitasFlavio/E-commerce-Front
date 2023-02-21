a46d55b847209511ab675837af17aeaf3f1cfc43
romain <romainmichel9@gmail.com>
2023-02-21 14:49:43 +0100

    clean merge

--- flutter_ecommerce/lib/router/router_notifier.dart
+++ flutter_ecommerce/lib/router/router_notifier.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/entities/user_role.dart';
 import 'package:flutter_ecommerce/screens/admin_page.dart';
-import 'package:flutter_ecommerce/screens/guest_page.dart';
 import 'package:flutter_ecommerce/screens/home_page.dart';
 import 'package:flutter_ecommerce/screens/login.dart';
 import 'package:flutter_ecommerce/screens/splash.dart';
@@ -10,7 +9,6 @@ import 'package:flutter_ecommerce/state/auth.dart';
 import 'package:flutter_ecommerce/state/permissions.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-// import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 class RouterNotifier extends AutoDisposeAsyncNotifier<void>
     implements Listenable {
